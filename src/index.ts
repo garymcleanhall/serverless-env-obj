aeb03cddafa26fe3d590259b8ca63bdd326b268a
Gary McLean Hall <gary@adaptivecode.com>
2021-05-16 12:16:57 -0400

    Forget the es6 equivalent of `module.exports = ...` so removing the index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,39 @@
-export * from './flatten'
-export * from './ServerlessEnvObj'
+import Serverless from 'serverless'
+import { flatten, MixedEnvironment } from './flatten'
+
+interface ProviderEnvironment {
+  configurationInput: {
+    provider: {
+      environment: MixedEnvironment
+    }
+  }
+}
+
+type Thunk = () => unknown
+
+class ServerlessEnvObj {
+  hooks: { [key: string]: Thunk }
+
+  constructor(private serverless: Serverless & ProviderEnvironment) {
+    this.hooks = {
+      initialize: this.transformEnv.bind(this),
+    }
+  }
+
+  transformEnv() {
+    this.serverless.service
+      .getAllFunctions()
+      .map((functionName) => this.serverless.service.getFunction(functionName))
+      .forEach(
+        (func) =>
+          (func.environment = func.environment
+            ? flatten(func.environment)
+            : func.environment)
+      )
+    this.serverless.configurationInput.provider.environment = flatten(
+      this.serverless.configurationInput.provider.environment
+    )
+  }
+}
+
+module.exports = ServerlessEnvObj
